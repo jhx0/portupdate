cc8ff1306e0e62bb450f34d963dabbe941cafe0b
jhx <jhx0x00@gmail.com>
2022-05-20 16:46:03 +0200

    general overhaul. emojis and color added. adapted to new website layout

--- portupdate.py
+++ portupdate.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 
 import re
+import emoji
 import requests
 from bs4 import BeautifulSoup
+from colorama import init, Fore, Style
 
+# the URL should not be changed
 url = 'https://www.freshports.org/'
 
+# max number of port updates to show
+# tune this to your needs
+max_updates = 10
+
 def showUpdates():
+    init()
+
     data = requests.get(url)
 
     html = BeautifulSoup(data.text, 'html.parser')
 
-    regexp = re.compile(r'href=*')
+    count = 0
 
-    for item in html.select('big > b'):
-        if not regexp.search(str(item)):
-            continue
+    for item in html.find_all('span', { 'class': 'element-details' }):
+        if count >= max_updates:
+            break
 
         name, version = item.text.split(" ")
-        print("{: <50}{}".format(name, version))
+        print("{} {: <50}{}".format(emoji.emojize(':package:'),
+                                Fore.GREEN + Style.BRIGHT + name, 
+                                Fore.RED + Style.BRIGHT + version))
+        count += 1 
+
+def main():
+    showUpdates()
 
 if __name__ == "__main__":
-    showUpdates()
+    main()
